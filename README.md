688c0439147c7befc1f253c68c5999aa58babc02
wangchao_windows <1601436878@qq.com>
2017-06-16 16:39:34 +0800

    window0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 这是一个Python代码仓库。
-这是一个在ubunru山的Python代码库
+这是一个在ubunru上的Python代码库
--- 爬虫/BeautifySoup/test.py
+++ 爬虫/BeautifySoup/test.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     result = requests.get("http://www.baidu.com")
     result.encoding = result.apparent_encoding
     soup = bs4.BeautifulSoup(result.text, "html.parser")
-    soup.prettify()
+    print(soup.prettify())
